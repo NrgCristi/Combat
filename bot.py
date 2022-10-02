796797e7dc432d7f236c4d7e21380053c1a91ef8
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-10-02 14:00:17 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,86 @@
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+import requests
+
+bot.commands()
+async def shop (ctx):
+    embed = discord.Embed(title="Item Shop")
+    embed.set_image(url="https://fortnite-api.com/v2/shop/br")
+    await ctx.send(embed=embed)
+
+bot = commands.Bot(commands_prefix='!')
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="AES Key")
+    embed.set_image(url="https://fortnite-api.com/v2/aes")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="Banners")
+    embed.set_image(url="https://fortnite-api.com/v1/banners")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="Banner Colors")
+    embed.set_image(url="https://fortnite-api.com/v1/banners/colors")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="Cosmetics List")
+    embed.set_image(url="https://fortnite-api.com/v2/cosmetics/br")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="New Cosmetics")
+    embed.set_image(url="https://fortnite-api.com/v2/cosmetics/br/new")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="BR Map")
+    embed.set_image(url="https://fortnite-api.com/v1/map")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="News")
+    embed.set_image(url="https://fortnite-api.com/v2/news")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="BR News")
+    embed.set_image(url="https://fortnite-api.com/v2/news/br")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="STW News")
+    embed.set_image(url="https://fortnite-api.com/v2/news/stw")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="Creative News")
+    embed.set_image(url="https://fortnite-api.com/v2/news/creative")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="Playlists")
+    embed.set_image(url="https://fortnite-api.com/v1/playlists")
+    await ctx.send(embed=embed)
+
+bot.commands()
+async def aes (ctx):
+    embed = discord.Embed(title="BR Stats")
+    embed.set_image(url="https://fortnite-api.com/v2/stats/br/v2/%7BaccountId%7D")
+    await ctx.send(embed=embed)
+
+bot.run('TOKEN')
